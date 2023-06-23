354a4584cc1f318d298fe26ffcf85ddc09e52d1e
Paul <97813342+Paul1404@users.noreply.github.com>
2023-06-23 23:08:04 +0200

    Update Registry-Changer.ps1

--- Registry-Changer.ps1
+++ Registry-Changer.ps1
@@ -1,4 +1,14 @@
+<#
+.SYNOPSIS
+This script allows to manipulate registry settings of all users profiles on the computer.
+.DESCRIPTION
+The script provides options to import settings from an XML file or enter them manually. 
+It also provides options to create a backup before applying the changes.
+#>
+
+# Custom functions to write output and errors
 function Write-CustomOutput {
+    [CmdletBinding()]
     param (
         [Parameter(ValueFromPipeline=$true)]
         [string]$Message,
@@ -10,13 +20,17 @@ function Write-CustomOutput {
 }
 
 function Write-CustomError {
+    [CmdletBinding()]
     param (
-        [string]$Message
+        [Parameter(ValueFromPipeline=$true)]
+        [string]$Message,
+        [string]$Color = "Red"
     )
     
-    Write-Host $Message -ForegroundColor Red
+    Write-Host $Message -ForegroundColor $Color
 }
 
+# Function to select a settings XML file
 function Get-SettingsFileDialog {
     $dialog = New-Object System.Windows.Forms.OpenFileDialog
     $dialog.InitialDirectory = $scriptRoot
@@ -31,6 +45,7 @@ function Get-SettingsFileDialog {
     }
 }
 
+# Function to read new parameters from the user
 function Get-NewParameters {
     $regPath = Read-Host -Prompt 'Enter the registry path'
     $valueName = Read-Host -Prompt 'Enter the value name'
@@ -43,7 +58,14 @@ function Get-NewParameters {
     }
 }
 
-function New-SettingsFile($parameters) {
+# Function to create a new settings file
+function New-SettingsFile {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        $parameters
+    )
+
     $settingsFiles = Get-ChildItem -Path $scriptRoot -Filter 'Settings*.xml' | Select-Object -ExpandProperty BaseName
     $maxNumber = ($settingsFiles | ForEach-Object { ($_ -replace '[^0-9]', '') -as [int] } | Sort-Object -Descending)[0]
 
@@ -52,9 +74,17 @@ function New-SettingsFile($parameters) {
     $settingsFilePath = Join-Path -Path $scriptRoot -ChildPath $settingsFileName
 
     $parameters | Export-Clixml -Path $settingsFilePath
+    return $settingsFilePath
 }
 
-function Update-UserProfiles($parameters) {
+# Function to update user profiles
+function Update-UserProfiles {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        $parameters
+    )
+
     Get-ChildItem "Registry::HKEY_USERS" | Where-Object { $_.PSChildName -match "S-1-5-21" } | ForEach-Object {
         $user = $_
         try {
@@ -78,8 +108,15 @@ function Update-UserProfiles($parameters) {
     }
 }
 
-function Confirm-Proceed {
-    $prompt = Read-Host "Do you want to proceed with these settings? (Y/N)"
+# Function to confirm user action
+function Confirm-Action {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Message
+    )
+
+    $prompt = Read-Host -Prompt $Message
     if ($prompt -eq 'Y') {
         $true
     } else {
@@ -87,33 +124,27 @@ function Confirm-Proceed {
     }
 }
 
-function Confirm-Backup {
-    $prompt = Read-Host "Do you want to backup the registry? (Y/N)"
-    if ($prompt -eq 'Y') {
-        $true
-    } else {
-        $false
-    }
-}
-
+# Function to backup registry
 function Backup-Registry {
-    $backupDir = Join-Path -Path $scriptRoot -ChildPath 'Backup'
-    if (!(Test-Path $backupDir)) {
-        New-Item -ItemType Directory -Path $backupDir | Out-Null
+    [CmdletBinding()]
+    param (
+        [string]$BackupDirectory = (Join-Path -Path $scriptRoot -ChildPath 'Backup')
+    )
+
+    if (!(Test-Path $BackupDirectory)) {
+        New-Item -ItemType Directory -Path $BackupDirectory | Out-Null
     }
     $timestamp = Get-Date -Format 'yyyyMMdd_HHmmss'
-    $backupFilePath = Join-Path -Path $backupDir -ChildPath "RegistryBackup_$timestamp.reg"
+    $backupFilePath = Join-Path -Path $BackupDirectory -ChildPath "RegistryBackup_$timestamp.reg"
 
     Write-CustomOutput "Starting backup..."
-    
-    # Start the backup operation in a separate process and wait for it to finish
     Start-Process -FilePath "regedit.exe" -ArgumentList "/E", "`"$backupFilePath`"" -NoNewWindow -Wait
-
     Write-CustomOutput "Backup complete."
+    
+    return $backupFilePath
 }
 
-
-
+# Main script
 Add-Type -AssemblyName System.Windows.Forms
 
 $scriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
@@ -121,17 +152,17 @@ $savedParameters = Get-SettingsFileDialog
 
 if ($null -eq $savedParameters) {
     $savedParameters = Get-NewParameters
-    New-SettingsFile -parameters $savedParameters
+    $settingsFilePath = New-SettingsFile -parameters $savedParameters
     Write-CustomOutput "Parameters saved to file: $settingsFilePath"
 }
 
 Write-CustomOutput 'Current saved parameters:'
 $savedParameters | Format-Table -AutoSize
 
-if (Confirm-Proceed) {
-    if (Confirm-Backup) {
-        Backup-Registry
-        Write-CustomOutput "Registry has been backed up."
+if (Confirm-Action -Message 'Do you want to proceed with these settings? (Y/N)') {
+    if (Confirm-Action -Message 'Do you want to backup the registry? (Y/N)') {
+        $backupFilePath = Backup-Registry
+        Write-CustomOutput "Registry has been backed up. Backup file: $backupFilePath"
     }
     Update-UserProfiles -parameters $savedParameters
     Write-CustomOutput "Script execution complete."

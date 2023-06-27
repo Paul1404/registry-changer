d1e8d7c881f7aad8aba029aca246a33e1a569cc8
Paul <97813342+Paul1404@users.noreply.github.com>
2023-06-27 20:29:58 +0200

    Update Elevated-Run.bat - Improved Documentation

--- Elevated-Run.bat
+++ Elevated-Run.bat
@@ -1,10 +1,28 @@
 @echo off
 
+REM SCRIPT: Run PowerShell Script with Elevated Privileges
+REM DESCRIPTION: This script allows you to run a PowerShell script with elevated privileges. 
+REM                It ensures that the user has the necessary permissions to execute the script.
+
+REM USAGE: 
+REM    1. Run the batch script.
+REM    2. Select the desired PowerShell script file using the file dialog.
+REM    3. The script will check if the selected PowerShell script exists.
+REM    4. If the script exists, it will generate a temporary PowerShell script to run with elevated privileges.
+REM    5. The selected PowerShell script will be executed with elevated privileges.
+REM    6. After execution, the temporary PowerShell script will be cleaned up.
+
+REM NOTES:
+REM    - This script requires PowerShell to be installed on the system.
+REM    - Ensure that you have the necessary permissions to execute scripts with elevated privileges.
+REM    - Make sure to select a valid PowerShell script file (.ps1 or .psm1) when prompted.
+REM    - This script was last updated on 2023-06-25.
+
 echo This script allows you to run a PowerShell script with elevated privileges.
 echo It ensures that the user has the necessary permissions to execute the script.
 echo.
 
-:: Use PowerShell to prompt for file selection
+REM Use PowerShell to prompt for file selection
 for /f "delims=" %%I in ('powershell.exe -noprofile -c "Add-Type -AssemblyName System.Windows.Forms; $openFileDialog = New-Object System.Windows.Forms.OpenFileDialog; $openFileDialog.Filter = 'PowerShell Script Files (*.ps1;*.psm1)|*.ps1;*.psm1|All Files (*.*)|*.*'; $openFileDialog.Title = 'Select the PowerShell script file'; if ($openFileDialog.ShowDialog() -eq 'OK') { $openFileDialog.FileName }"') do set "psFile=%%I"
 
 if "%psFile%" == "" (
@@ -23,20 +41,20 @@ if exist "%psFile%" (
     echo Generating a temporary PowerShell script to run with elevated privileges...
     echo.
 
-    :: Generate a temporary PowerShell script
+    REM Generate a temporary PowerShell script
     echo Start-Process powershell.exe -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File "%psFile%"' > "%temp%\run_elevated_temp.ps1"
 
     echo Running the PowerShell script with elevated privileges...
     echo.
 
-    :: Run the temporary PowerShell script
+    REM Run the temporary PowerShell script
     powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%temp%\run_elevated_temp.ps1"
 
     echo.
     echo Cleaning up the temporary PowerShell script...
     echo.
 
-    :: Clean up the temporary PowerShell script
+    REM Clean up the temporary PowerShell script
     del "%temp%\run_elevated_temp.ps1"
 
     echo.
@@ -50,3 +68,4 @@ if exist "%psFile%" (
     echo.
     PAUSE
 )
+
